9ad8fe3283d70ac756bbe965ce85422a1c609d96
negarin-nb <nnb.se21@gmail.com>
2023-06-19 22:24:21 +0330

    update

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
-import { Box, Button, Stack, Toolbar, Typography } from "@mui/material";
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React, { useEffect } from "react";
+import HeaderLogedin from "./headerLogedin";
+import HeaderNotLogedin from "./headerNotLogedin";
+import { useAuth } from "../contex/authContext";
 
 const TypographyStyle = {
   fontWeight: 600,
@@ -21,77 +22,15 @@ const ButtonStyle = {
 };
 
 export default function Header() {
-  //const theme = useTheme();
-  //const isMobile = useMediaQuery(theme.breakPoints.down("xs"))
-  return (
-    <Stack direction="row" sx={{ pt: "20px" }}>
-      <Stack direction="row" spacing={0} sx={{ flex: 1 }}>
-        <Button
-          sx={ButtonStyle}
-          variant="outlined"
-          component={Link}
-          to="/register"
-        >
-          عضویت
-        </Button>
-        <Button sx={ButtonStyle} component={Link} to="/login">
-          ورود
-        </Button>
-      </Stack>
+  const authCtx = useAuth();
+  useEffect(() => {
+    if (!authCtx.token) {
+      const token = localStorage.getItem("token");
+      if (token) {
+        authCtx.setUserToken(JSON.parse(token));
+      }
+    }
+  }, []);
 
-      <Stack
-        direction="row"
-        dir="rtl"
-        spacing={{ xs: "0px", sm: "30px", md: "30px", lg: "40px" }}
-        sx={{ flex: 2, justifyContent: "center", alignItems: "center" }}
-      >
-        <Typography
-          sx={TypographyStyle}
-          variant="h3"
-          component={NavLink}
-          to="/"
-        >
-          خانه
-        </Typography>
-        <Typography
-          sx={TypographyStyle}
-          variant="h3"
-          component={NavLink}
-          to="/products"
-        >
-          محصولات
-        </Typography>
-        <Typography
-          sx={TypographyStyle}
-          variant="h3"
-          component={NavLink}
-          to="/price"
-        >
-          قیمت آنلاین
-        </Typography>
-        <Typography
-          sx={TypographyStyle}
-          variant="h3"
-          component={NavLink}
-          to="/about"
-        >
-          درباره ما
-        </Typography>
-        <Typography
-          sx={TypographyStyle}
-          variant="h3"
-          component={NavLink}
-          to="/contact"
-        >
-          تماس با ما
-        </Typography>
-      </Stack>
-      <Stack direction="row" sx={{ flex: 1, justifyContent: "flex-end" }}>
-        <img
-          style={{ width: "100%", maxWidth: "120px" }}
-          src={require("../assets/images/Logo601.png")}
-        />
-      </Stack>
-    </Stack>
-  );
+  return <>{!!authCtx.token ? <HeaderLogedin /> : <HeaderNotLogedin />}</>;
 }
