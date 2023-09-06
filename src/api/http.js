b28a53554daa1f8dc5b56a38c4a7f4df28c7fd52
negarin-nb <nnb.se21@gmail.com>
2023-09-06 12:43:36 +0330

    update

--- src/api/http.js
+++ src/api/http.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 function setJwt(jwt) {
-  console.log(JSON.parse(jwt));
+  //console.log(JSON.parse(jwt));
   if (jwt)
     axios.defaults.headers.common["Authorization"] = `Bearer ${JSON.parse(
       jwt
     )}`;
-  console.log(axios.defaults.headers.common["Authorization"]);
+  //console.log(axios.defaults.headers.common["Authorization"]);
 }
 
 export default { setJwt };
